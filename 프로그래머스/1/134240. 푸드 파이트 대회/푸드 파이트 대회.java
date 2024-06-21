ae360a60c0de6da1ed1c044ca1188e825238f602
seowonson <144876148+seowonn@users.noreply.github.com>
2024-06-21 11:22:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 8.23 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] food) {
+        
+        StringBuilder  sb = new StringBuilder();
+        
+        for(int i = 0; i < food.length; i++) {
+            for(int j = 0; j < food[i] / 2; j++){
+                sb.append(i);
+            }
+        }
+        
+        return sb.toString() + "0" + sb.reverse().toString();
+    }
+}
