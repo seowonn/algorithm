041d1f91abd0a9ec92cd169a320d801fcac503c1
seowonson <144876148+seowonn@users.noreply.github.com>
2024-07-07 16:20:07 +0900

    [level 2] Title: [3차] n진수 게임, Time: 17.55 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,25 +1,21 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        String answer = "";
-        String converted = "";
+        StringBuilder answer = new StringBuilder();
+        StringBuilder converted = new StringBuilder();
         
         int num = 0;
         while(converted.length() < p + m * t) {
-            String convertedNum = convertNum(num++, n);
+            String convertedNum = Integer.toString(num++, n);
             for(String s : convertedNum.split("")){
-                converted += s;
+                converted.append(s);
                 if(converted.length() >= p + m * t){
                     break;
                 }
                 if((converted.length() - p) % m == 0){
-                    answer += s.toUpperCase();
+                    answer.append(s.toUpperCase());
                 } 
             }
         }
-        return answer;
-    }
-    
-    public static String convertNum(int num, int n) {
-        return Integer.toString(num, n);
+        return answer.toString();
     }
 }
