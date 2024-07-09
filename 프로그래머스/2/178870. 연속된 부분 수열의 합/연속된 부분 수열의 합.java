5d1649564d1674dc94e3a1e8bac0774f6c54c618
seowonson <144876148+seowonn@users.noreply.github.com>
2024-07-09 14:35:58 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,59 +1,23 @@
-import java.util.*;
-
-class SubSum implements Comparable<SubSum>{
-    int startIdx;
-    int endIdx;
-    int length;
-    
-    public SubSum(int start, int end) {
-        this.startIdx = start;
-        this.endIdx = end;
-        this.length = end - start + 1;
-    }
-    
-    public int compareTo(SubSum o){
-        if(o.length == this.length) {
-            if(o.startIdx > this.startIdx) {
-                return -1;
-            }
-            return 1;
-        } else if(o.length > this.length) {
-            return -1;
-        }
-        return 1;
-    }
-}
-
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        int[] answer = {};
+        int[] answer = {0, 1_000_000};
+
+        int total = 0;
+        int start = 0;
         
-        int pointer1 = 0, pointer2 = 1;
-        int sum = sequence[pointer1];
-        
-        PriorityQueue<SubSum> pq = new PriorityQueue<>();
-        while(pointer1 <= pointer2 && pointer1 >= 0 && pointer2 < sequence.length) {
-            if(sum == k) {
-                pq.offer(new SubSum(pointer1, pointer2));
-                sum += sequence[pointer2++];
-            } else if (sum < k) {
-                sum += sequence[pointer2++];
-            } else if(sum > k) {
-                sum -= sequence[pointer1++];
+        for(int end = 0; end < sequence.length; end++) {
+            total += sequence[end];
+            
+            while(total > k) {
+                total -= sequence[start++];
             }
-        }
-        
-        while(pointer1 < sequence.length && sum > k) {
-            sum -= sequence[pointer1++];
-        }
-        
-        if(sum == k) {
-            pq.offer(new SubSum(pointer1, pointer2));
-        }
-        
-        if(!pq.isEmpty()) {
-            SubSum result = pq.poll();
-            answer = new int[]{result.startIdx, result.endIdx - 1};
+            
+            if(total == k) {
+                if((end - start) < (answer[1] - answer[0])) {
+                    answer[0] = start;
+                    answer[1] = end;
+                }
+            }
         }
         
         return answer;
