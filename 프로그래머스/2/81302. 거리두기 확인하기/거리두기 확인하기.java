b09c0aa4f000f7e2356be8b02e399c8da5f1c30b
seowonson <144876148+seowonn@users.noreply.github.com>
2024-08-06 13:06:24 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.32 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -2,83 +2,71 @@ import java.util.*;
 
 class Solution {
     
-    static final int LENGTH = 5;
+    static int[][] dirs = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    static int LENGTH = 5;
     
-    // 4방향
-    static int[][] dirs = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
-        
     public int[] solution(String[][] places) {
         
-        // 답안 배열
         int[] answer = new int[LENGTH];
         
-        // 배열 길이 만큼 반복
-        for(int idx = 0; idx < LENGTH; idx++) {
-            
-            // 규칙 이행 여부
-            boolean correct = true;
-            
-            // 현재 대기실 맵
-            String[] place = places[idx];
-            
-            // 응시자 주변 맨해튼 거리 2까지 탐색해서 규칙 이행 여부를 구한다. 
-            outerLoop:
+        for(int room = 0; room < LENGTH; room++) {
+            boolean followed = true;
+            // 전파 진원지 : 응시자 탐색
+            loop1: 
             for(int i = 0; i < LENGTH; i++) {
                 for(int j = 0; j < LENGTH; j++) {
-                    // 최단 거리 탐색 BFS 이용
-                    if(place[i].charAt(j) == 'P') { // 응시자 위치 발견
-                        // 응시자 위치 기준 2만 탐색하면 되서 응시자 위치만 넘겨줌.
-                        if(!bfs(i, j, place)) {
-                            correct = false;
-                            break outerLoop;
+                    if(places[room][i].charAt(j) == 'P'){ // 응시자 발견
+                        if(!checkIfFollowed(i, j, places[room])){
+                            followed = false;
+                            break loop1;
                         }
                     }
                 }
             }
-            answer[idx] = correct ? 1 : 0;
+            answer[room] = followed ? 1 : 0;
         }
         return answer;
     }
     
-    public static boolean bfs(int r, int c, String[] map) {
+    public static boolean checkIfFollowed(int r, int c, String[] place) {
         
-        // bfs 재료 1. 방문여부 저장 배열
-        boolean[][] visited = new boolean[LENGTH][LENGTH];
-        
-        // bfs 재료 2. queue
-        Queue<int[]> queue = new LinkedList<>();
-        
-        queue.offer(new int[]{r, c, 0});
+        boolean[][] visited = new boolean[place.length][place.length];
         visited[r][c] = true;
         
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{r, c, 0});
+        
         while(!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int currentR = pos[0], currentC = pos[1], dist = pos[2];
             
-            // 현재 위치(출발 지점 제외)에 응시자가 존재. 맨해튼 거리가 2이하인 경우 : 규칙 위반
-            if(dist> 0 && dist <= 2 && map[currentR].charAt(currentC) == 'P') {
+            int[] pos = queue.poll();
+            
+            // 다른 응시자가 있는 경우 : false
+            if(pos[2] > 0 && pos[2] <= 2 && place[pos[0]].charAt(pos[1]) == 'P') {
                 return false;
             }
             
-            // 거리 2까지만 계속 탐색
-            if(dist < 2) {
-                // 4방향 탐색
-                for(int[] dir : dirs) {
-                    int nextR = currentR + dir[0];
-                    int nextC = currentC + dir[1];
-
-                    // 인덱스 범위 검사
-                    if(isInRange(nextR, nextC) && !visited[nextR][nextC]) {
-                        if(map[nextR].charAt(nextC) == 'X') {  // 파티션 통과 불가. 탐색 중지
-                            continue;
-                        }    
-                        visited[nextR][nextC] = true;
-                        queue.offer(new int[]{nextR, nextC, dist + 1});
+            // 빈 테이블만 있는 경우 
+            if(pos[2] >= 2) {
+                continue;
+            }
+            
+            for(int[] dir : dirs) {
+                int nextR = pos[0] + dir[0];
+                int nextC = pos[1] + dir[1];
+                
+                if(isInRange(nextR, nextC) && !visited[nextR][nextC]) {
+                    
+                    // 파티션이 있는 경우 : true
+                    if(place[nextR].charAt(nextC) == 'X') {
+                        continue;
                     }
+                    
+                    visited[nextR][nextC] = true;
+                    queue.offer(new int[]{nextR, nextC, pos[2] + 1});
                 }
             }
         }
-        return true;    // 거리 두기 규칙 준수 
+        return true;
     }
     
     public static boolean isInRange(int r, int c) {
