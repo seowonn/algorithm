6e7d8bef68599dbc6101c0e67fe40700c5facc92
seowonson <144876148+seowonn@users.noreply.github.com>
2024-08-21 15:30:13 +0900

    [level 3] Title: 등산코스 정하기, Time: 258.49 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -1,93 +1,89 @@
 import java.util.*;
+import java.util.stream.*;
 
-class Edge implements Comparable<Edge>{
-    int num;
-    int cost;
+class Edge {
+    int node;
+    int weight;
     
-    public Edge(int num, int cost) {
-        this.num = num;
-        this.cost = cost;
+    public Edge(int node, int weight) {
+        this.node = node;
+        this.weight = weight;
+    }
+}
+
+class Entry implements Comparable<Entry> {
+    int intensity;
+    int node;
+    
+    public Entry(int intensity, int node) {
+        this.intensity = intensity;
+        this.node = node;
     }
     
     @Override
-    public int compareTo(Edge other) {
-        return Integer.compare(this.cost, other.cost);   
+    public int compareTo(Entry other) {
+        // 결과 음수 : this가 우선. 즉 오름차순
+        return this.intensity - other.intensity;
     }
 }
 
 class Solution {
-    
-    static int[] staticGates;
-    static int[] staticSummits;
-    
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        Set<Integer> summitSet = new HashSet<>();
-        for(int summit : summits) {
-            summitSet.add(summit);
-        }
-        
-        // 양방향 그래프 생성
+        // 입력값을 내가 쓰기 편한 구조로 바꾼다. => 무방향(양방향) 그래프로 바꾼다.
+        Arrays.sort(summits);
+        Set<Integer> summitSet = IntStream.of(summits).boxed().collect(Collectors.toSet());
         Map<Integer, List<Edge>> graph = new HashMap<>();
-        for(int i = 1; i <= n; i++) {
-            graph.put(i, new ArrayList<Edge>());
-        }
         for(int[] path : paths) {
+            graph.putIfAbsent(path[0], new LinkedList<>());
+            graph.putIfAbsent(path[1], new LinkedList<>());
+            
             graph.get(path[0]).add(new Edge(path[1], path[2]));
             graph.get(path[1]).add(new Edge(path[0], path[2]));
         }
         
-        // 다익스트라 알고리즘 수행
-        int[] intensity = new int[n + 1];
-        Arrays.fill(intensity, Integer.MAX_VALUE);
-        intensity[0] = 0;
-        
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        
-        // pq로 모든 노드들을 도는데 각 노드들은 maxCost라고 
-        // 본인이 거쳐온 경로 상의 소요 시간 중에서 가장 큰 값을 끌고 이동한다.
-        // 산봉우리에 도착 시, 본인이 갖고 있는 maxCost과 비교하여 더 작은 값을 갱신해준다. 
-        
-        // 왜 pq에 한 방에 gates를 다 넣는가?
-        // 탐색 조건으로 새로 계산한 값이 본래보다 작아야 하는데, 이 계산 자체를
-        // 출입구 후보에선 진행하지 않게 하기 위해 0으로 설정해준다. 
+        // 모든 출입구를 우선순위 큐에 삽입한다. 
+        // 모든 출입구는 intenisty를 0으로 설정하고, 재방문을 방지하기 위해
+        // visited의 gate 위치는 다 0으로 방문 처리한다.
+        Queue<Entry> hq = new PriorityQueue<>();
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
         for(int gate : gates) {
-            pq.offer(new Edge(gate, 0));
-            intensity[gate] = 0;
+            hq.add(new Entry(0, gate));
+            visited[gate] = 0;
         }
         
-        while(!pq.isEmpty()) {
-            Edge cur = pq.poll();
+        // intensity를 기준으로 다익스트라를 진행한다.
+        while(!hq.isEmpty()) {
+            Entry entry = hq.poll();
             
-            // 현재 노드로 가기 위해 소요되는 비용이 현재 위치까지의 이미 적힌
-            // cost보다 크다면 계산을 진행할 필요가 없다. 따라서 continue 처리
-            if(cur.cost > intensity[cur.num]) continue;
+            // entry의 노드 번호가 산봉우리 중 하나라면 탐색을 중단 (hq에 넣지 않음)한다.
+            // 또한 현재 entry의 intensity가 방문 배열의 해당 위치에 저장된 값보다 크다면 굳이 탐색을 더하지 않는다.
+            if(entry.intensity > visited[entry.node] || summitSet.contains(entry.node)){
+                continue;
+            }
             
-            if(summitSet.contains(cur.num)) continue;
-            
-            for(Edge next : graph.get(cur.num)) {
-                // 현재 노드의 비용과 그 다음 이동 노드까지의 소요 비용을 비교하여
-                // 최대 비용을 갱신한다.
-                int maxIntensity = Math.max(intensity[cur.num], next.cost);
-                if(intensity[next.num] > maxIntensity) {
-                    intensity[next.num] = maxIntensity;
-                    pq.offer(new Edge(next.num, maxIntensity));
+            for(Edge edge : graph.get(entry.node)){
+                int nextIntensity = Math.max(edge.weight, entry.intensity);
+                // 현재 탐색 대상인 edge.node까지의 역대 최대 intensity 값과 현재 구한 intensity값을 비교한다. 
+                if(nextIntensity < visited[edge.node]) {
+                    // 현재 구한 intensity 값이 더 작다면 그 값으로 visited를 갱신해준다. 
+                    visited[edge.node] = nextIntensity;
+                    // Entry는 hq에 들어갈, 탐색 대상이 되는 노드 후보이다. 
+                    // Entry는 역대 최대 intensity 값과 해당 노드로 구성된다. 
+                    hq.add(new Entry(nextIntensity, edge.node));
                 }
             }
         }
-            
-        // 최소 intensity를 가지는 산봉우리 찾기
-        int minSummit = -1;
-        int minIntensity = Integer.MAX_VALUE;
-
+        
+        // 다익스트라 완료 후 산봉우리들을 순회하며 정답을 찾는다.
+        int[] minIntensity = {0, Integer.MAX_VALUE};
         for(int summit : summits) {
-            if(intensity[summit] < minIntensity){
-                minIntensity = intensity[summit];
-                minSummit = summit;
-            } else if(intensity[summit] == minIntensity && summit < minSummit) {
-                minSummit = summit;
+            if(minIntensity[1] > visited[summit]) {
+                minIntensity[0] = summit;
+                minIntensity[1] = visited[summit];
             }
-
         }
-        return new int[]{minSummit, minIntensity};
+        
+        return minIntensity;
     }
 }
