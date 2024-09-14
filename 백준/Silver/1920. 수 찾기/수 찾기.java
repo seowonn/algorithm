1eba684e5ddc4bc8f9aa6e7e8db80b763f83cb32
seowonson <144876148+seowonn@users.noreply.github.com>
2024-09-14 15:22:24 +0900

    [Silver IV] Title: 수 찾기, Time: 1196 ms, Memory: 56396 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,53 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] cards;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+    int N = Integer.parseInt(br.readLine());
+    int[] A = createArray(br.readLine().split(" "));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+    int M = Integer.parseInt(br.readLine());
+    int[] B = createArray(br.readLine().split(" "));
 
-
-        cards = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int cardsNumHeHave = Integer.parseInt(br.readLine());
-        int[] cardsHeHave = new int[cardsNumHeHave];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < cardsNumHeHave; i++) {
-            cardsHeHave[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(cards);
-
-        for (int i = 0; i < cardsNumHeHave; i++) {
-            System.out.println(has(cardsHeHave[i]) ? 1 : 0);
-        }
-
+    Arrays.sort(A);
+    for(int num : B) {
+      if(binarySearch(A, num, 0, A.length - 1)) {
+        System.out.println(1);
+      } else {
+        System.out.println(0);
+      }
     }
+  }
 
-    private static boolean has(int target) {
-        int left = 0;
-        int right = cards.length - 1;
+  private static boolean binarySearch(int[] arr, int target, int left, int right) {
+    boolean exists;
 
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            if(cards[mid] == target){
-                return true;
-            } else if(cards[mid] < target){
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return false;
+    // target 값이 존재하지 않을 경우, left나 right 값이 서로 역전 될 수 있다. 이를 대비하여
+    // 재귀 탈출 장치를 마련해야 한다.
+    if(left > right) return false;
+
+    int mid = (left + right) / 2;
+    if(arr[mid] < target) {
+      exists = binarySearch(arr, target, mid + 1, right);
+    } else if(arr[mid] == target) {
+      return true;
+    } else {
+      exists = binarySearch(arr, target, left, mid - 1);
     }
+    return exists;
+  }
+
+  public static int[] createArray(String[] str) {
+    int[] array = new int[str.length];
+    for(int i = 0; i < str.length; i++) {
+      array[i] = Integer.parseInt(str[i]);
+    }
+    return array;
+  }
+
 }
