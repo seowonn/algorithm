e84b6b3367dc1ba4ba66b1bddfef377a794cf655
seowonson <144876148+seowonn@users.noreply.github.com>
2024-09-15 17:39:01 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,23 +1,19 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         int p1 = 0, p2 = 0;
-        int targetIdx = 0;
         
-        while(targetIdx < goal.length) {
-            boolean atLeastmatched = false;
-            while(p1 < cards1.length && goal[targetIdx].equals(cards1[p1])) {
-                targetIdx++;
+        for(int i = 0; i < goal.length; i++) {
+            
+            String target = goal[i];
+            
+            if(p1 < cards1.length && target.equals(cards1[p1])) {
                 p1++;
-                atLeastmatched = true;
-            }
-            while(p2 < cards2.length && targetIdx < goal.length && goal[targetIdx].equals(cards2[p2])) {
-                targetIdx++;
+            } else if(p2 < cards2.length && target.equals(cards2[p2])) {
                 p2++;
-                atLeastmatched = true;
-            }
-            if(!atLeastmatched) {
+            } else {
                 return "No";
             }
+            
         }
         return "Yes";
     }
