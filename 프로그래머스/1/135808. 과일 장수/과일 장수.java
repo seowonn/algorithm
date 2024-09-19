78a97b488c7a0512f24c90e3b18971576c1ad1fd
seowonson <144876148+seowonn@users.noreply.github.com>
2024-09-20 07:29:14 +0900

    [level 1] Title: 과일 장수, Time: 83.43 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -6,11 +6,9 @@ class Solution {
         
         Arrays.sort(score);
         int idx = 1;
-        for(int i = score.length - 1; i >= 0; i--) {
-            if(idx % m == 0) {
-                answer += score[i] * m;
-            }
-            idx++;
+        // 마지막부터 m개씩 묶어 가장 낮은 점수를 계산한다. 
+        for(int i = score.length - m; i >= 0; i-=m) {
+            answer += score[i] * m;
         }
         
         return answer;
