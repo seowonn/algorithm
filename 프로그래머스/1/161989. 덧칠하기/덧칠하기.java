67e73c5897d444ab039a3ee06bd5f3b73bd87b7d
seowonson <144876148+seowonn@users.noreply.github.com>
2024-09-20 07:41:49 +0900

    [level 1] Title: 덧칠하기, Time: 1.14 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,12 +1,6 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 1;
-        // n(페인트 길이)만큼의 배열 생성
-        int[] arr = new int[n];
-        for(int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-        
+        int answer = 1;       
         int end = section[0] + m;
         for(int i = 0; i < section.length; i++) {
             if(section[i] >= end) {
